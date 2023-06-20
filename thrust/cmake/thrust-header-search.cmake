8452c764cc8d772314169e99811535f3a9108cfe
Ben Frederickson <ben@benfrederickson.com>
2023-06-20 15:11:33 -0700

    add NO_CMAKE_FIND_ROOT_PATH to overcome conda build errors

--- thrust/cmake/thrust-header-search.cmake
+++ thrust/cmake/thrust-header-search.cmake
@@ -1,6 +1,7 @@
 # Parse version information from version.h:
 unset(_THRUST_VERSION_INCLUDE_DIR CACHE) # Clear old result to force search
 find_path(_THRUST_VERSION_INCLUDE_DIR thrust/version.h
+  NO_CMAKE_FIND_ROOT_PATH
   NO_DEFAULT_PATH # Only search explicit paths below:
   PATHS
     "${CMAKE_CURRENT_LIST_DIR}/../.."            # Source tree
